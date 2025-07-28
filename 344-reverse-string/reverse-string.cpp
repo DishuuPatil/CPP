0ec24186f6b751b80ed303f92844691b4fab0d8e
DishuuPatil <70340528+DishuuPatil@users.noreply.github.com>
2025-07-28 22:53:32 +0530

    Time: 0 ms (100.00%) | Memory: 27.3 MB (51.18%) - LeetSync

--- 344-reverse-string/reverse-string.cpp
+++ 344-reverse-string/reverse-string.cpp
@@ -1,27 +1,14 @@
 class Solution {
 public:
     void reverseString(vector<char>& s) {
-        
-       int i = 0, j = s.size() - 1;
-	while (i < j) swap(s[i++], s[j--]);
+       int first = 0, last = s.size()-1;
+       while(first<last)
+       {
+        char temp = s[first];
+        s[first] = s[last];
+        s[last] = temp;
+        first++;
+        last--;
+       } 
     }
 };
-
-// here we just swap the letters using two pointers.
-/* //create a stack
-        stack<char> st;
-        //loop through the string, to add all the string elements in stack
-        for(int i=0;i<s.size();i++){
-            //create a variable to add the values
-            char ch = s[i];
-            st.push(ch);
-        }
-        //empty the original string
-        s.clear();
-        //add the stack elements to the string
-        while(!st.empty()){
-            char ch = st.top();
-            //reversed
-            s.push_back(ch);
-            st.pop();
-        }*/

ec8415bfa90a476954fa1ec8fc664413309d4f72
DishuuPatil <70340528+DishuuPatil@users.noreply.github.com>
2025-07-28 23:19:51 +0530

    Time: 0 ms (100.00%) | Memory: 27.4 MB (16.66%) - LeetSync

--- 344-reverse-string/reverse-string.cpp
+++ 344-reverse-string/reverse-string.cpp
@@ -1,15 +1,15 @@
 class Solution {
 public:
     void reverseString(vector<char>& s) {
-        //int i = 0;
-        s = reverseString(0, s.size()-1, s);
+        //using recursion;
+        reverseString(0, s.size()-1, s);
        }
 
-    vector<char> reverseString(int first, int last, vector<char> &string)
+    void reverseString(int first, int last, vector<char> &string)
     {
         if(first>=last)
-            return string;
+            return;
         swap(string[first], string[last]);
-        return reverseString(first+1, last-1, string);
+        reverseString(first+1, last-1, string);
     }
 };

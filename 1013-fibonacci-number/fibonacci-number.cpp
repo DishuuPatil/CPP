de56018c27102a34196935a2de7f6611f4c1fd9b
DishuuPatil <70340528+DishuuPatil@users.noreply.github.com>
2025-07-30 08:20:52 +0530

    Time: 3 ms (66.34%) | Memory: 7.8 MB (64.08%) - LeetSync

--- 1013-fibonacci-number/fibonacci-number.cpp
+++ 1013-fibonacci-number/fibonacci-number.cpp
@@ -4,14 +4,13 @@ public:
         if(n==0)return 0;
         if(n==1)return 1;
 
-        long long first = 0, sum = 0, second = 1;
-        return fibonacci(first, second, 2, n, sum);
+        long long first = 0, second = 1;
+        return fibonacci(first, second, 2, n);
     }
-    long long fibonacci(long long first, long long second, int i, int n, long long sum)
+    long long fibonacci(long long first, long long second, int i, int n)
     {
-        if(i>n) 
-            return sum;
-        sum = first+second;
-        return fibonacci(second, sum, ++i, n, sum);
+        if(i>=n) 
+            return first+second;
+        return fibonacci(second, first+second, ++i, n);
     }
 };
